11bd4cc5118ad0e3edc0878e6a67024e9d1fb3ae
ahyar4y <ahyar4y08@gmail.com>
2020-08-20 14:00:31 +0700

    main branch

--- lib/models/imageData.dart
+++ lib/models/imageData.dart
@@ -1,44 +1,7 @@
-import 'dart:io';
-// import 'dart:math';
-// import 'package:image/image.dart';
-import 'package:firebase_ml_vision/firebase_ml_vision.dart';
-
 class ImageData {
   String temp = '';
   String image;
   List<String> ingredients = [];
-
-  // Image preProcess() {
-  //   Image img = decodeImage(File(this.image).readAsBytesSync());
-  //   double factor = max(1, 1024.0/img.width);
-  //   int newHeight = factor.toInt() * img.height;
-  //   img = copyResize(img, height: newHeight);
-  //   img = contrast(img, 150);
-  //   img = grayscale(img);
-
-  //   return img;
-  // }
-
-  Future readImage() async {
-    final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(File(this.image));
-    final TextRecognizer ocr = FirebaseVision.instance.textRecognizer();
-    final VisionText imageText = await ocr.processImage(visionImage);
-    RegExp regex = RegExp(r".*?(\(([^)]+)\))?,");
-    String str = '';
-
-    for (TextBlock block in imageText.blocks) {
-      for (TextLine line in block.lines) {
-        for (TextElement element in line.elements) {
-          str+=element.text;
-        }
-      }
-    }
-    print(str);
-
-    ocr.close();
-
-    // printIngredients();
-  }
 
   void setIngredients() {
     ingredients.add('Milk Chocolate (Sugar, Cocoa Butter, Chocolate, Lactose, Skim Milk, Milkfat, Soy Lecithin, Artificial Flavor)');
